62aa697d0f98aede65b87d35aa9a9c1023e23a81
Eugene <zato.prosto@gmail.com>
2019-10-14 00:42:11 +0300

    add constants (path)

---
+++ src/constants.tsx
@@ -0,0 +1,22 @@
+export const PATH = {
+    "HOME": {
+        "LINK": "/",
+        "LABEL": "Home"
+    },
+    "DOCTORS": {
+        "LINK": "/doctors",
+        "LABEL": "Doctors"
+    },
+    "PATIENTS": {
+        "LINK": "/patients",
+        "LABEL": "Patients"
+    },
+    "PROCEDURES": {
+        "LINK": "/procedures",
+        "LABEL": "Procedures"
+    },
+    "ROOMS": {
+        "LINK": "/rooms",
+        "LABEL": "Rooms"
+    },
+};
