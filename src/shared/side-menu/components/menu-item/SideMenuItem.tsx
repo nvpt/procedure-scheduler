fef35505e5fcb98ffe5ef189b5f4d863e25d1f41
Eugene <zato.prosto@gmail.com>
2019-10-15 00:57:56 +0300

    SideMenu List and Item billets

---
+++ src/shared/side-menu/components/menu-item/SideMenuItem.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import cn from "./SideMenuItem.module.css";
+
+export default class SideMenuItem extends React.Component{
+  state = {};
+
+  render() {
+    return <div className={cn.SideMenu}>Side Menu Item</div>;
+  }
+}
---
+++ src/shared/side-menu/components/menu-list/SideMenuList.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import cn from "./SideMenuList.module.css";
+
+export default class SideMenuList extends React.Component{
+  state = {};
+
+  render() {
+    return <div className={cn.SideMenu}>Side Menu List</div>;
+  }
+}
