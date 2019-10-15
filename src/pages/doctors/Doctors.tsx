000906c0cd36f5032c48e72c688cb0111a2e57a7
Eugene <zato.prosto@gmail.com>
2019-10-15 14:40:29 +0300

    removed custom table component; add doctors table on mock

--- src/pages/doctors/Doctors.tsx
+++ src/pages/doctors/Doctors.tsx
@@ -3,10 +3,12 @@ import React from 'react'
 import cn from './doctors.module.css'
 import { DoctorsList } from '../../mock/DoctorsMock'
 import DoctorInterface from '../../interfaces/DoctorInterface'
+import { Table } from 'react-bootstrap'
 
 interface DoctorsProps {}
 interface DoctorsState {
     doctors: Array<DoctorInterface>
+    emptyPlaceholder: string
 }
 
 export default class Doctors extends React.Component<
@@ -15,10 +17,37 @@ export default class Doctors extends React.Component<
 > {
     state: DoctorsState = {
         doctors: DoctorsList,
+        emptyPlaceholder: 'List Is Empty',
     }
     render() {
         console.log('Doctors.tsx__render >>> DoctorsList: ', DoctorsList)
+        const { doctors, emptyPlaceholder } = this.state
 
-        return <div>Here will be doctors table</div>
+        if (doctors && doctors.length) {
+            return (
+                <div className={cn.doctors}>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {doctors.map((doctor) => {
+                                return (
+                                    <tr key={doctor.Id}>
+                                        <td>{doctor.Id}</td>
+                                        <td>{doctor.Name}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </Table>
+                </div>
+            )
+        } else {
+            return <div className={cn.doctors}>{emptyPlaceholder}</div>
+        }
     }
 }
