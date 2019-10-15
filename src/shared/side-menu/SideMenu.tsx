5f9140c135bad8e06158045d25c5b0dbefe99c33
Eugene <zato.prosto@gmail.com>
2019-10-15 15:54:17 +0300

    change logo)

--- src/shared/side-menu/SideMenu.tsx
+++ src/shared/side-menu/SideMenu.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import cn from './sideMenu.module.css'
 import { SideMenuList } from './components/menu-list/SideMenuList'
+import logo from '../../logo.png'
 
 interface Props {}
 
 export const SideMenu: React.FC<Props> = () => {
     return (
         <div className={cn.sideMenu}>
+            <div className={cn.logoWrap}>
+                <img src={logo} alt='' width={'40'} height={'40'} />
+            </div>
             <SideMenuList />
         </div>
     )
