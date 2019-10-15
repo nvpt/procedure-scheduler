271da185a1b3952ea1bb993c8b0726294948bbbb
Eugene <zato.prosto@gmail.com>
2019-10-15 10:36:38 +0300

    minor fix

--- src/shared/side-menu/components/menu-item/SideMenuItem.tsx
+++ src/shared/side-menu/components/menu-item/SideMenuItem.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import cn from './SideMenuItem.module.css'
 import MenuItemInterface from '../../../../interfaces/MenuItemInterface'
+
 interface Props {
     item: MenuItemInterface
-    className: string
+    className?: string
 }
 
 export const SideMenuItem: React.FC<Props> = ({ item, className }) => {
