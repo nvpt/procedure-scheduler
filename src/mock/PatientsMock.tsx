ec5123dbfcc6792b98184110f3916698940a21e8
Eugene <zato.prosto@gmail.com>
2019-10-16 15:55:24 +0300

    change date format for datepicker

--- src/mock/PatientsMock.tsx
+++ src/mock/PatientsMock.tsx
@@ -5,12 +5,12 @@ export const PatientsList: PatientInterface[] = [
         Id: 1,
         Name: "John Doe",
         Sex: "Male",
-        DayOfBirth: "02.10.1999"
+        DayOfBirth: "1999-12-30"
     },
     {
         Id: 2,
         Name: "Rita Smith",
         Sex: "Female",
-        DayOfBirth: "01.11.1975"
+        DayOfBirth: "1975-09-20"
     },
 ];
