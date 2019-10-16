edd3bde7eba27621ff42b57931275d1ede5631ff
Eugene <zato.prosto@gmail.com>
2019-10-16 22:11:25 +0300

    just styles rework

--- src/shared/header/Header.tsx
+++ src/shared/header/Header.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import cn from './header.module.css'
+import logo from '../../logo.png'
 
 export default class Header extends React.Component {
     state = {}
 
     render() {
-        return <div className={cn.header}>Procedure Scheduler</div>
+        return <div className={cn.header}>
+            <div className={cn.logoWrap}>
+                <img src={logo} alt='' width={'30'} height={'30'} />
+            </div>
+            <div>Procedure Scheduler</div></div>
     }
 }
