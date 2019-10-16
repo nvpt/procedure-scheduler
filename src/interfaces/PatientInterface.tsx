07a65ea866dd6a4788ed6279869259554b8caa06
Eugene <zato.prosto@gmail.com>
2019-10-16 16:07:30 +0300

    ID should be required

--- src/interfaces/PatientInterface.tsx
+++ src/interfaces/PatientInterface.tsx
@@ -1,7 +1,7 @@
 export type sexType = 'Male' | 'Female' | null
 
 export default interface PatientInterface {
-    Id: number | null
+    Id: number
     Name: string
     Sex?: sexType
     DayOfBirth?: string | ''
