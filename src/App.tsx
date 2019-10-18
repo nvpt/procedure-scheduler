a3cdb3a501402a5a5aaa4adcadc2825b82f4d970
Eugene <zato.prosto@gmail.com>
2019-10-18 15:54:24 +0300

    change router type

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { HashRouter as Router, Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { patientsActions } from './store-global/reducers/PatientsReducer'
 
